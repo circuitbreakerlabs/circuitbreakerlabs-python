d16f8dafe241fa66a28538428ecaf5c89b5c7d47
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-11-29 15:43:11 -0800

    add generation app to flake

--- flake.nix
+++ flake.nix
@@ -17,19 +17,47 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (nixpkgs) lib;
+
+        build = with pkgs; [
+          uv
+          openapi-python-client
+        ];
+
+        postProcessing = with pkgs; [
+          ruff
+          prettier
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
           LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc ];
 
-          buildInputs = with pkgs; [
-            uv
-			openapi-python-client
-            mypy
-            ruff
-            prettier
-          ];
+          buildInputs = with pkgs; [ mypy ] ++ build ++ postProcessing;
         };
+
+        apps.generate =
+          let
+            generateScript = pkgs.writeShellApplication {
+              name = "generate";
+              runtimeInputs = build ++ postProcessing;
+              text = ''
+                echo "Using openapi-python-client version: $(openapi-python-client --version)"
+                echo "Using config file at: $PWD/config.yaml"
+                echo "Using additional templates from: $PWD/templates"
+
+                openapi-python-client generate --url https://api.circuitbreakerlabs.ai/v1/openapi.json \
+                  --output-path "$PWD" \
+                  --overwrite \
+                  --config "$PWD/config.yaml" \
+                  --custom-template-path="$PWD/templates" \
+                  --meta uv
+              '';
+            };
+          in
+          {
+            type = "app";
+            program = lib.getExe generateScript;
+          };
       }
     );
 }
